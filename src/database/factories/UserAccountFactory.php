98d9e8d990735e79b6380f9ac3a174a383037a96
Devid Duma <dduma19@epoka.edu.al>
2021-05-20 19:11:23 +0200

    UserAccount model, seeder, factory

---
+++ src/database/factories/UserAccountFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\UserAccount;
+use Faker\Generator as Faker;
+
+$factory->define(UserAccount::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
