ef23f2bea641e8959775b4dda8fb4180ae0761ce
Devid Duma <dduma19@epoka.edu.al>
2021-05-20 19:06:48 +0200

    PatientDiagnosis model, seeder, factory

---
+++ src/database/factories/PatientDiagnosisFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\PatientDiagnosis;
+use Faker\Generator as Faker;
+
+$factory->define(PatientDiagnosis::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
