6555b8f22c918c9652d9c2a44eb942eca3fbf575
Devid Duma <dduma19@epoka.edu.al>
2021-05-20 19:05:16 +0200

    Patient model, seeder, factory

---
+++ src/app/Http/Controllers/HealthRecordController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\HealthRecord;
+use Illuminate\Http\Request;
+
+class HealthRecordController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\HealthRecord  $healthRecord
+     * @return \Illuminate\Http\Response
+     */
+    public function show(HealthRecord $healthRecord)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\HealthRecord  $healthRecord
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(HealthRecord $healthRecord)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\HealthRecord  $healthRecord
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, HealthRecord $healthRecord)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\HealthRecord  $healthRecord
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(HealthRecord $healthRecord)
+    {
+        //
+    }
+}
