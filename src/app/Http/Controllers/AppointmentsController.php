6cd9f3f44c54f5f1b7475fd555942157991e44b1
Devid Duma <dduma19@epoka.edu.al>
2021-05-20 19:02:16 +0200

    Diagnosis model, seeder, factory, controller

---
+++ src/app/Http/Controllers/AppointmentsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Appointments;
+use Illuminate\Http\Request;
+
+class AppointmentsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Appointments  $appointments
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Appointments $appointments)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Appointments  $appointments
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Appointments $appointments)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Appointments  $appointments
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Appointments $appointments)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Appointments  $appointments
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Appointments $appointments)
+    {
+        //
+    }
+}
---
+++ src/database/factories/DiagnosisFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Diagnosis;
+use Faker\Generator as Faker;
+
+$factory->define(Diagnosis::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
