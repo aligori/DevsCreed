3f9d90e615e8d690e33fc0e81f3db83209b961a6
Devid Duma <dduma19@epoka.edu.al>
2021-05-20 19:04:06 +0200

    HealthRecord model, seeder, factory

---
+++ src/database/factories/HealthRecordFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\HealthRecord;
+use Faker\Generator as Faker;
+
+$factory->define(HealthRecord::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ src/database/migrations/2021_05_20_170321_create_health_records_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHealthRecordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('health_records', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('health_records');
+    }
+}
