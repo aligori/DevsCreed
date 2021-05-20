a280b4cadc851552c75253fb69a7f16087b38766
Devid Duma <dduma19@epoka.edu.al>
2021-05-20 19:00:52 +0200

    appointment model, seeder and factory

---
+++ src/database/factories/AppointmentsFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Appointments;
+use Faker\Generator as Faker;
+
+$factory->define(Appointments::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
