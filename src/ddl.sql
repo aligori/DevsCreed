f3378ef093a484510aff5cd4b583903bd8c1531d
Devid Duma <dduma19@epoka.edu.al>
2021-06-02 20:38:31 +0200

    full ddl

--- src/ddl.sql
+++ src/ddl.sql
@@ -9,12 +9,62 @@ CREATE TABLE `diagnosis` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
+-- ecms.`product category` definition
+
+CREATE TABLE `product category` (
+                                    `category_id` bigint unsigned NOT NULL AUTO_INCREMENT,
+                                    `name` varchar(40) NOT NULL,
+                                    PRIMARY KEY (`category_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
 -- ecms.`role` definition
 
 CREATE TABLE `role` (
                         `role_id` bigint unsigned NOT NULL AUTO_INCREMENT,
                         `name` varchar(40) NOT NULL,
                         PRIMARY KEY (`role_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- ecms.service definition
+
+CREATE TABLE `service` (
+                           `name` varchar(40) NOT NULL,
+                           `description` varchar(100) DEFAULT NULL,
+                           `price` int unsigned NOT NULL,
+                           `service_id` bigint unsigned NOT NULL AUTO_INCREMENT,
+                           PRIMARY KEY (`service_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- ecms.product definition
+
+CREATE TABLE `product` (
+                           `price` bigint unsigned NOT NULL,
+                           `sell_price` bigint unsigned NOT NULL,
+                           `quantity` int unsigned NOT NULL,
+                           `product_id` bigint unsigned NOT NULL AUTO_INCREMENT,
+                           `name` varchar(100) NOT NULL,
+                           `description` text,
+                           `category_id` bigint unsigned NOT NULL,
+                           PRIMARY KEY (`product_id`),
+                           KEY `product_FK` (`category_id`),
+                           CONSTRAINT `product_FK` FOREIGN KEY (`category_id`) REFERENCES `product category` (`category_id`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- ecms.`transaction` definition
+
+CREATE TABLE `transaction` (
+                               `date` datetime NOT NULL,
+                               `client` varchar(100) NOT NULL,
+                               `total` int unsigned NOT NULL,
+                               `transaction_id` bigint unsigned NOT NULL AUTO_INCREMENT,
+                               `service_id` bigint unsigned DEFAULT NULL,
+                               PRIMARY KEY (`transaction_id`),
+                               KEY `transaction_FK` (`service_id`),
+                               CONSTRAINT `transaction_FK` FOREIGN KEY (`service_id`) REFERENCES `service` (`service_id`) ON DELETE SET NULL ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
@@ -63,6 +113,18 @@ CREATE TABLE `patient diagnosis` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
+-- ecms.`product instance` definition
+
+CREATE TABLE `product instance` (
+                                    `product_id` bigint unsigned NOT NULL,
+                                    `transaction_id` bigint unsigned NOT NULL,
+                                    PRIMARY KEY (`product_id`,`transaction_id`),
+                                    KEY `product_instance_FK_1` (`transaction_id`),
+                                    CONSTRAINT `product_instance_FK` FOREIGN KEY (`product_id`) REFERENCES `product` (`product_id`) ON DELETE RESTRICT ON UPDATE CASCADE,
+                                    CONSTRAINT `product_instance_FK_1` FOREIGN KEY (`transaction_id`) REFERENCES `transaction` (`transaction_id`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
 -- ecms.staff definition
 
 CREATE TABLE `staff` (
@@ -89,11 +151,17 @@ CREATE TABLE `appointment` (
                                `time` datetime NOT NULL,
                                `assigned_to` bigint unsigned DEFAULT NULL,
                                `booked_by` bigint unsigned DEFAULT NULL,
+                               `transaction_id` bigint unsigned NOT NULL,
+                               `service_id` bigint unsigned DEFAULT NULL,
                                PRIMARY KEY (`a_id`),
                                KEY `appointment_FK` (`assigned_to`),
                                KEY `appointment_FK_1` (`booked_by`),
+                               KEY `appointment_FK_2` (`transaction_id`),
+                               KEY `appointment_FK_3` (`service_id`),
                                CONSTRAINT `appointment_FK` FOREIGN KEY (`assigned_to`) REFERENCES `staff` (`employee_id`) ON DELETE SET NULL ON UPDATE CASCADE,
-                               CONSTRAINT `appointment_FK_1` FOREIGN KEY (`booked_by`) REFERENCES `patient` (`patient_id`) ON DELETE SET NULL ON UPDATE CASCADE
+                               CONSTRAINT `appointment_FK_1` FOREIGN KEY (`booked_by`) REFERENCES `patient` (`patient_id`) ON DELETE SET NULL ON UPDATE CASCADE,
+                               CONSTRAINT `appointment_FK_2` FOREIGN KEY (`transaction_id`) REFERENCES `transaction` (`transaction_id`) ON DELETE RESTRICT ON UPDATE CASCADE,
+                               CONSTRAINT `appointment_FK_3` FOREIGN KEY (`service_id`) REFERENCES `service` (`service_id`) ON DELETE SET NULL ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
