ea98785985df51ff4d6f9d6b848136d850151814
drabeli18 <drabeli18@epoka.edu.al>
2021-06-05 00:40:43 +0200

    New changes made to model classes

--- Code/model/employee.class.php
+++ Code/model/employee.class.php
@@ -26,24 +26,29 @@
                                     `uuid`
             */
             var_export($arr);
-            //$stmt = null;
+            //The stmt = null is a good coding practice.
+            $stmt = null;
             
         }
 
         public static function modifyEmployeeData($employee_id, $field, $change) {
             /*modify -> specify which field to modify, specify new value to be entered and then executing*/ 
            
-            $query = "UPDATE `staff` SET $field = ? WHERE id = ?";
+            $query = "UPDATE `staff` SET ? = ? WHERE id = ?";
             
             $stmt = $this->$dbh->prepare($query);
-            $stmt->execute([$change, $employee_id]);
-            //$stmt = null;
+            $stmt->execute([$field, $change, $employee_id]);
+            $stmt = null;
 
         }
 
         public function changeStatus($employee_id) {
             
             $currentStatus = getCurrentStatus($employee_id);
+
+            // if condition, if status = 1 then it is active so make it 0
+            // 0 represents an inactive account
+            // if 0 reactivate
 
             if ($currentStatus == 1) {
                 $query = "UPDATE `staff` SET `status` = 0 WHERE id = ?";
@@ -53,10 +58,11 @@
 
             $stmt = $this->$dbh->prepare($query);
             $stmt->execute([$employee_id]);
-            //$stmt = null;
+            $stmt = null;
 
         }
 
+        // This function is used at changeStatus in order to get the current status of user
         private function getCurrentStatus($employee_id){
 
             $stmt = $this->$dbh->prepare("SELECT `status` FROM `staff` WHERE employee_id = ?");
@@ -64,6 +70,6 @@
             $status = $stmt->fetch(PDO::FETCH_ASSOC);
 
             var_export($status);
-            //$stmt = null;
+            $stmt = null;
         } 
     }
---
+++ Code/model/product.class.php
@@ -0,0 +1,54 @@
+<?php
+    require_once('../db_conn.php');
+
+    class Product {
+
+        private $dbh = (new Database())->get_connection();
+
+        public static function getProduct($product_id) {
+            
+            //Prepare query and fetch result
+            $stmt = $this->$dbh->prepare("SELECT * FROM `product` WHERE product_id = ?");
+            $stmt->execute([$product_id]);
+            $arr = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            if(!$arr) exit('No rows');
+
+            /*
+            What will be returned: 
+                                    `price` 
+                                    `sell_price` 
+                                    `quantity`
+                                    `product_id` 
+                                    `name` 
+                                    `description` 
+                                    `category_id`
+            */
+            var_export($arr);
+            //The stmt = null is a good coding practice.
+            $stmt = null;
+            
+        }
+
+        public static function modifyProducts($product_id, $field, $change) {
+            /*modify -> specify which field to modify, specify new value to be entered and then executing*/ 
+           
+            $query = "UPDATE `product` SET ? = ? WHERE id = ?";
+            
+            $stmt = $this->$dbh->prepare($query);
+            $stmt->execute([$field, $change, $employee_id]);
+            $stmt = null;
+
+        }
+
+        public static function addToQuantity($product_id, $quantity) {
+            /*modify -> specify which field to modify, specify new value to be entered and then executing*/ 
+           
+            $query = "UPDATE `staff` SET `quantity` = `quantity` + ? WHERE id = ?";
+            
+            $stmt = $this->$dbh->prepare($query);
+            $stmt->execute([$quantity, $employee_id]);
+            $stmt = null;
+
+        }
+
+    }
